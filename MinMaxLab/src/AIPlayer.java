31f582909aa8ca48e18c542cc7d58bbdd85f861c
jtfakult <jtfakult@wpi.edu>
2016-08-30 19:22:13 -0400

    Updated with example of how to implement MatrixData structure

--- MinMaxLab/src/AIPlayer.java
+++ MinMaxLab/src/AIPlayer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class AIPlayer extends Player{
 	public AIPlayer(String n, int t, int l)
@@ -6,8 +7,24 @@ public class AIPlayer extends Player{
 	}
 
 	@Override
-	public Move getMove(StateTree state) {
-		// TODO Auto-generated method stub
+	public Move getMove(StateTree state)
+	{
+		MatrixData data = new MatrixData();
+		
+		//This is how you get user and opponent data.
+		MatrixData myCurrentState = data.getMatrixData(state, true, 4);
+		MatrixData theirCurrentState = data.getMatrixData(state, false, 4);
+		
+		/*
+		 * Example usage, list all user sequences of length n
+		 */
+		int n = 3;
+		for (InARow seq : data.sequences.get(n-1)) //n-1 because threes are in index 2
+		{
+			String direction = data.dirToString(seq.dir);
+			System.out.println("Sequence of size " + n + " in a row found at row: " + seq.row + ", col: " + seq.col + " and pointing: " + direction + "!");
+		}
+		
 		return null;
 	}
 }
