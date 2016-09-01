ba2531887afff50bc8afdad370e38d8243804324
DIRECTIONNZ <ZhangNan@130.215.169.119>
2016-09-01 18:21:46 -0400

    Finished checking vertical and horizontal

--- MinMaxLab/src/AIPlayer.java
+++ MinMaxLab/src/AIPlayer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class AIPlayer extends Player{
 	public AIPlayer(String n, int t, int l)
@@ -26,12 +27,32 @@ public class AIPlayer extends Player{
 			}
 		}
 		
+		int[] horizontalOneFree = new int[winNumber];
+		for (int i = 0; i < horizontalOneFree.length; i++){
+			if (i == (horizontalOneFree.length - 1)){
+				horizontalOneFree[i] = Integer.MAX_VALUE;
+			}
+			else{
+				horizontalOneFree[i] = 2 * (i + 1);
+			}
+		}
+		
+		int[] horizontalTwoFree = new int[winNumber];
+		for (int i = 0; i < horizontalTwoFree.length; i++){
+			if (i == (horizontalTwoFree.length - 1)){
+				horizontalTwoFree[i] = Integer.MAX_VALUE;
+			}
+			else{
+				horizontalTwoFree[i] = 3 * (i + 1);
+			}
+		}
+		
 		
 		//check vertically with free space on top
-		for (int i = 0; i < state.columns; i++){
+		for (int j = 0; j < state.columns; j++){
 			int continuous1 = 0;
 			int continuous2 = 0;
-			for (int j = 0; j < state.rows; j++){
+			for (int i = 0; i < state.rows; i++){
 				if (state.boardMatrix[i][j] == 0){
 					if (continuous1 > 0){
 						utility += verticalWeight[continuous1 - 1];
@@ -51,6 +72,63 @@ public class AIPlayer extends Player{
 				}
 			}
 		}
+		
+		//check horizontally
+		for (int i = 0; i < state.rows; i++){
+			int continuous1 = 0;
+			int continuous2 = 0;
+			for (int j = 0; j < state.columns; j++){
+				if (state.boardMatrix[i][j] == 1){
+					continuous2 = 0;
+					continuous1 += 1;
+					while (j < (state.columns - 1) && state.boardMatrix[i][j+1] == 1){
+						continuous1 += 1;
+						j += 1;
+					}
+					if (j < state.columns - 1 && state.boardMatrix[i][j+1] == 0 && state.boardMatrix[i][j-continuous1] == 0){
+						utility += horizontalTwoFree[continuous1 - 1];
+					}
+					else if (j < state.columns - 1 && state.boardMatrix[i][j+1] == 0){
+						utility += horizontalOneFree[continuous1 -1];
+					}
+					else if (state.boardMatrix[i][j-continuous1] == 0){
+						utility += horizontalOneFree[continuous1 -1];
+					}
+				}
+				else if (state.boardMatrix[i][j] == 2){
+					continuous1 = 0;
+					continuous2 += 1;
+					while (j < (state.columns - 1) && state.boardMatrix[i][j+1] == 1){
+						continuous2 += 1;
+						j += 1;
+					}
+					if (j < state.columns - 1 && state.boardMatrix[i][j+1] == 0 && state.boardMatrix[i][j-continuous2] == 0){
+						utility -= horizontalTwoFree[continuous2 - 1];
+					}
+					else if (j < state.columns - 1 && state.boardMatrix[i][j+1] == 0){
+						utility -= horizontalOneFree[continuous2 -1];
+					}
+					else if (state.boardMatrix[i][j-continuous2] == 0){
+						utility -= horizontalOneFree[continuous2 -1];
+					}
+				}
+			}
+		}
+		
+		
+		//check diagonally
+		for(int i = winNumber - 1; i < state.rows; i++){
+			ArrayList<Integer> list = new ArrayList<>();
+			int rowsBelow = 0;
+			int columnIndex = 0;
+			while(i - rowsBelow >= 0){
+				list.add(state.boardMatrix[i-rowsBelow][columnIndex]);
+				columnIndex++;
+				rowsBelow++;
+			}
+			//TODO
+		}
+		
 		return utility;
 	}
 }
