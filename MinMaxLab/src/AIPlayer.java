8ce9d1bb40497a8a0b3c0783c96f81db0b4d220c
DIRECTIONNZ <ZhangNan@130.215.169.119>
2016-09-04 03:57:56 -0400

    Heuristic Finished without testing

--- MinMaxLab/src/AIPlayer.java
+++ MinMaxLab/src/AIPlayer.java
@@ -47,6 +47,36 @@ public class AIPlayer extends Player{
 			}
 		}
 		
+		int[] diagonalBot = new int[winNumber];
+		for (int i = 0; i < diagonalBot.length; i++){
+			if (i == (diagonalBot.length - 1)){
+				diagonalBot[i] = Integer.MAX_VALUE;
+			}
+			else{
+				diagonalBot[i] = 2 * (i + 1);
+			}
+		}
+		
+		int[] diagonalTop = new int[winNumber];
+		for (int i = 0; i < diagonalTop.length; i++){
+			if (i == (diagonalTop.length - 1)){
+				diagonalTop[i] = Integer.MAX_VALUE;
+			}
+			else{
+				diagonalTop[i] = 2 * (i + 1);
+			}
+		}
+		
+		int[] diagonal_Underneath = new int[winNumber];
+		for (int i = 0; i < diagonal_Underneath.length; i++){
+			if (i == (diagonal_Underneath.length - 1)){
+				diagonal_Underneath[i] = Integer.MAX_VALUE;
+			}
+			else{
+				diagonal_Underneath[i] = 3 * (i + 1);
+			}
+		}
+		
 		
 		//check vertically with free space on top
 		for (int j = 0; j < state.columns; j++){
@@ -116,8 +146,12 @@ public class AIPlayer extends Player{
 		}
 		
 		
-		//check diagonally
+		//check diagonal with negative slope
 		for(int i = winNumber - 1; i < state.rows; i++){
+			int continuous1 = 0;
+			int continuous2 = 0;
+			int underneath1 = 0;
+			int underneath2 = 0;
 			ArrayList<Integer> list = new ArrayList<>();
 			int rowsBelow = 0;
 			int columnIndex = 0;
@@ -126,7 +160,432 @@ public class AIPlayer extends Player{
 				columnIndex++;
 				rowsBelow++;
 			}
-			//TODO
+			
+			//check bottom
+			if (list.get(list.size() - 1) == 0){
+				for (int index = (list.size() - 2); index >= 0; index--){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (state.boardMatrix[i - index - 1][index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (state.boardMatrix[i - index - 1][index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalBot[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalBot[continuous2 - 1];
+					}
+				}
+			}
+			
+			//check top
+			if (list.get(0) == 0){
+				continuous1 = 0;
+				continuous2 = 0;
+				underneath1 = 0;
+				underneath2 = 0;
+				for (int index = 1; index < list.size(); index++){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (i - index - 1 >= 0 && state.boardMatrix[i - index - 1][index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (i - index - 1 >= 0 && state.boardMatrix[i - index - 1][index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalTop[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalTop[continuous2 - 1];
+					}
+				}
+			}
+			
+		}
+		for(int i = 1; i <= state.columns - winNumber; i++){
+			int continuous1 = 0;
+			int continuous2 = 0;
+			int underneath1 = 0;
+			int underneath2 = 0;
+			ArrayList<Integer> list = new ArrayList<>();
+			int columnProgress = 0;
+			while(i + columnProgress < state.columns){
+				list.add(state.boardMatrix[state.rows - columnProgress - 1][i + columnProgress]);
+				columnProgress++;
+			}
+			//check bottom
+			if (list.get(list.size() - 1) == 0){
+				for (int index = (list.size() - 2); index >= 0; index--){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i + index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i + index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalBot[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalBot[continuous2 - 1];
+					}
+				}
+			}
+			
+			//check top
+			if (list.get(0) == 0){
+				continuous1 = 0;
+				continuous2 = 0;
+				underneath1 = 0;
+				underneath2 = 0;
+				for (int index = 1; index < list.size(); index++){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i + index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i + index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalTop[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalTop[continuous2 - 1];
+					}
+				}
+			}
+			
+		}
+		
+		//check positive slope
+		for(int i = winNumber - 1; i < state.rows; i++){
+			int continuous1 = 0;
+			int continuous2 = 0;
+			int underneath1 = 0;
+			int underneath2 = 0;
+			ArrayList<Integer> list = new ArrayList<>();
+			int rowsBelow = 0;
+			int columnIndex = state.columns - 1;
+			while(i - rowsBelow >= 0){
+				list.add(state.boardMatrix[i-rowsBelow][columnIndex]);
+				columnIndex--;
+				rowsBelow++;
+			}
+			
+			//check bottom
+			if (list.get(list.size() - 1) == 0){
+				for (int index = (list.size() - 2); index >= 0; index--){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (state.boardMatrix[i - index - 1][state.columns - 1 - index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (state.boardMatrix[i - index - 1][state.columns - 1 - index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalBot[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalBot[continuous2 - 1];
+					}
+				}
+			}
+			
+			//check top
+			if (list.get(0) == 0){
+				continuous1 = 0;
+				continuous2 = 0;
+				underneath1 = 0;
+				underneath2 = 0;
+				for (int index = 1; index < list.size(); index++){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (i - index - 1 >= 0 && state.boardMatrix[i - index - 1][state.columns - 1 - index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (i - index - 1 >= 0 && state.boardMatrix[i - index - 1][state.columns - 1 - index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalTop[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalTop[continuous2 - 1];
+					}
+				}
+			}
+			
+		}
+		for(int i = state.columns - 2; i >= winNumber - 1; i--){
+			int continuous1 = 0;
+			int continuous2 = 0;
+			int underneath1 = 0;
+			int underneath2 = 0;
+			ArrayList<Integer> list = new ArrayList<>();
+			int columnProgress = 0;
+			while(i - columnProgress >= 0){
+				list.add(state.boardMatrix[state.rows - columnProgress - 1][i - columnProgress]);
+				columnProgress++;
+			}
+			//check bottom
+			if (list.get(list.size() - 1) == 0){
+				for (int index = (list.size() - 2); index >= 0; index--){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i - index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i - index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalBot[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalBot[continuous2 - 1];
+					}
+				}
+			}
+			
+			//check top
+			if (list.get(0) == 0){
+				continuous1 = 0;
+				continuous2 = 0;
+				underneath1 = 0;
+				underneath2 = 0;
+				for (int index = 1; index < list.size(); index++){
+					if (list.get(index) == 1){
+						if(continuous2 == 0){
+							continuous1 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous1 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i - index] != 0){
+								underneath1 = 1;
+							}
+						}
+					}
+					else if (list.get(index) == 2){
+						if(continuous1 == 0){
+							continuous2 += 1;
+						}
+						else{
+							break;
+						}
+						if(continuous2 == 1){
+							if (state.boardMatrix[state.rows - index - 2][i - index] != 0){
+								underneath2 = 1;
+							}
+						}
+					}
+				}
+				if (underneath1 == 1){
+					utility += diagonal_Underneath[continuous1 - 1];
+				}
+				else if(underneath2 == 1){
+					utility -= diagonal_Underneath[continuous2 - 1];
+				}
+				else{
+					if (continuous1 != 0){
+						utility += diagonalTop[continuous1 - 1];
+					}
+					if (continuous2 != 0){
+						utility -= diagonalTop[continuous2 - 1];
+					}
+				}
+			}
+			
 		}
 		
 		return utility;
