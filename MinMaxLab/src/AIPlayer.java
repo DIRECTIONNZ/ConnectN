ae6638c2ef2017f1d50665f122c684545104c481
jtfakult <jtfakult@wpi.edu>
2016-09-04 12:46:32 -0400

    First update to AIPlayer minimax algorithm

--- MinMaxLab/src/AIPlayer.java
+++ MinMaxLab/src/AIPlayer.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
-import java.util.Stack;
+import java.util.Collections;
+import java.util.Random;
+import java.lang.Math;
+//import java.util.Stack;
 
 public class AIPlayer extends Player
 {
@@ -11,18 +14,27 @@ public class AIPlayer extends Player
 	@Override
 	public Move getMove(StateTree state)
 	{
-		MatrixData data = new MatrixData();
+		//MatrixData data = new MatrixData();
 		
-		Move move = minimax_wrapper(state, 2000); //tree then time limit
-		
+		Move move = minimax_wrapper(state, this.timeLimit * 1000);
+		//Random r = new Random();
+		//Move move = new Move(false, r.nextInt() & 7);
+		/*for (int i=0; i<state.boardMatrix.length; i++)
+		{
+			for (int j=0; j<state.boardMatrix[i].length; j++)
+			{
+				System.out.print(state.boardMatrix[i][j]);
+			}
+			System.out.println("--");
+		}*/
 		return move;
 	}
 	
 	private Move minimax_wrapper(StateTree state, long timeLimit)
 	{
 		long startTime = System.currentTimeMillis();
-		ArrayList<Move> moves = new ArrayList<Move>();
-		ArrayList<Integer> heuristics = minimax(state, 0, 0, timeLimit, startTime); //Change this time limit! Only searches to depth 1 right now
+		
+		ArrayList<Integer> heuristics = minimax(state, 0, 0, timeLimit, startTime); //Only searches to depth 1 right now
 		
 		int max=0, maxIndex=0;
 		for (int index = 0; index < heuristics.size(); index++)
@@ -40,32 +52,70 @@ public class AIPlayer extends Player
 	//DO NOT CALL DIRECTLY
 	private ArrayList<Integer> minimax(StateTree state, int depth, int maxDepth, long timeLimit, long startTime)
 	{
+		boolean userMove = ((depth+this.turn) % 2) == 1;
+		//Recursive base case. Telling us to stop DFS if we are at the iterative depth limit or if time is up
 		if (depth <= maxDepth || (System.currentTimeMillis() - startTime) > timeLimit)
 		{
-			int decay = 1;///(depth + 1)
 			ArrayList<Integer> values = new ArrayList<Integer>(state.columns);
 			
-			for (int choice = 0; choice < state.columns; choice++, decay -= 0)
+			//Return all child node values of this state tree
+			for (int choice = 0; choice < state.columns; choice++)
 			{
 				Move move = new Move(false, choice);
 				state.makeMove(move);
-				values.add(getHeuristic(state) * decay);
+				if (userMove)
+				{
+					values.add(getHeuristic(state));
+				}
+				else
+				{
+					values.add(getHeuristic(state));
+				}
 				undoMove(state, move);
 			}
 			
 			return values;
 		}
 		
+		//values of child nodes
 		ArrayList<Integer> values = new ArrayList<Integer>();
+		
+		//values of child's child nodes (the max(branchValues) will be put into values each loop)
 		ArrayList<Integer> branchValues = new ArrayList<Integer>();
 		
+		//In this loop we iterate over each of the columns and check the minimax value for that tree
 		for (int choice = 0; choice < state.columns; choice++)
 		{
+			//Prepare to make a move and put it into the column $choice
 			Move move = new Move(false, choice);
 			state.makeMove(move);
-			branchValues = minimax(state, depth+1, maxDepth, timeLimit, startTime);
+			
+			//make the move, check the minimax, then undo the move
+			//If this level on the tree is our move just check the regular tree
+			//If it is their move then we will use our multiple heuristic pruning algorithm
+			if (userMove)
+			{
+				branchValues = minimax(state, depth+1, maxDepth, timeLimit, startTime); //Get values of this new state trees children
+			}
+			else
+			{
+				branchValues = minimax(state, depth+1, depth+1, timeLimit, startTime); //change 3rd param soon. Need variable for defensedepth
+				pruneBadGuesses(branchValues, .5);
+				
+				for (int i=0; i<branchValues.size(); i++)
+				{
+					if (branchValues.get(i) > Integer.MIN_VALUE)
+					{
+						Move move2 = new Move(false, i);
+						state.makeMove(move2);
+						branchValues = minimax(state, depth+1, maxDepth, timeLimit, startTime); //Get values of this new state trees children
+						undoMove(state, move2);
+					}
+				}
+			}
 			undoMove(state, move);
 			
+			//Get the max value of all these branch nodes
 			int max = 0;
 			for (int b : branchValues)
 			{
@@ -83,36 +133,47 @@ public class AIPlayer extends Player
 	
 	private int getHeuristic(StateTree state)
 	{
-		MatrixData data = new MatrixData();
-		data.getMatrixData(state, true, 0);
+		/*MatrixData data = new MatrixData();
+		data.getMatrixData(state, this.turn, 0);
 		
-		return data.sequences.get(0).size() + data.sequences.get(1).size() * 2 + data.sequences.get(2).size() * 3;
+		int sum = 0;
+		int index = 1;
+		for (ArrayList<InARow> d : data.sequences)
+		{
+			System.out.println("index " + index + " " + d.size());
+			sum += d.size() * index;
+		}
+		
+		return sum;*/
+		Random r = new Random();
+		return r.nextInt();
 	}
 	
+	//Undoes the last move made so we can recurse back up the minimax tree 
 	private void undoMove(StateTree state, Move move)
 	{
 		if(move.pop)
 		{
-			if(turn == 1)
+			if(state.turn == 1)
 				state.pop1 = false;
-			if(turn == 2)
+			if(state.turn == 2)
 				state.pop2 = false;
-			for(int i=0; i<state.rows-1; i++)
+			for (int i = state.rows-2; i>=0; i--) //(int i=0; i<state.rows-1; i++)
 			{
-				state.boardMatrix[i][move.column] = state.boardMatrix[i-1][move.column];
+				state.boardMatrix[i][move.column] = state.boardMatrix[i+1][move.column];
 			}
 			state.boardMatrix[state.rows-1][move.column] = turn;
-			turn = Math.abs(turn-3);
+			state.turn = Math.abs(state.turn-3);
 			return;
 		}
 		else
 		{
-			for (int i=0; i<state.rows; i++)
+			for (int i=state.rows -1; i >= 0; i--)
 			{
 				if (state.boardMatrix[i][move.column] != 0)
 				{
 					state.boardMatrix[i][move.column] = 0;
-					turn = Math.abs(turn-3);
+					state.turn = Math.abs(state.turn-3);
 					return;
 				}
 			}
@@ -237,5 +298,27 @@ public class AIPlayer extends Player
 		}
 		
 		return utility;
+	}
+	
+	private void pruneBadGuesses(ArrayList<Integer> branches, double aggressiveness)
+	{
+		aggressiveness = Math.max(0, Math.min(1,  aggressiveness));
+		ArrayList<Integer> sorted = new ArrayList<Integer>(branches);
+		Collections.sort(sorted);
+		
+		int size = sorted.size();
+		int cutoffIndex = (int)Math.round(aggressiveness * (size - 1));
+		int cutoff = branches.get(cutoffIndex);
+		int count = 0;
+		for (int i=0; i<branches.size(); i++)
+		{
+			if (branches.get(i) < cutoff)
+			{
+				count++;
+				branches.set(i, Integer.MIN_VALUE);
+			}
+		}
+		
+		System.out.println("pruned " + count + " guesses");
 	}
 }
